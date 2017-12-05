ddee165882a1b802576b99c10bdd0e90bb676ec8
wangjiannan <904089747@qq.com>
2017-12-05 22:08:14 +0800

    add sidebar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,6 @@
       <div class="main">
         <div class="head"></div>
         <div class="main-inner">
-          
           <div class="left">
             <div class="header">
               <p>arronwjn</p>
@@ -20,7 +19,20 @@
               <li>关于</li>
             </ul>
           </div>
-          <div class="bar"></div>
+          <div class="sidebar">
+            <div class="sidebar-inner">
+              <div class="sidebar-top">
+                <div class="avater">
+                  <img src="../assets/arronwjn.jpg" alt="">
+                </div>
+                <p>arronwjn</p>
+              </div>
+              <div class="sidebar-min">
+                <p>吉林 长春 长春工业大学</p>
+                <p>待我代码打完，娶你为妻可好</p>
+              </div>
+            </div>
+          </div>
           <div class="right">
             <router-view></router-view>
           </div>
@@ -108,16 +120,59 @@
 .router-link-active{
   color:rgb(255,160,0);
 }
-.bar{
-  width: 15px;
-  height:100%;
-  background-color: #e9eaee;
-}
 .right{
+  position: absolute;
+  top:0;
+  right:0;
   display: inline-block;
-  width:100%;
+  width:76%;
   height:100%;
   border-radius: 5px;
+}
+.sidebar{
+  position: absolute;
+  top:0;
+  left:0;
+  width:250px;
+  margin-top: 250px;
+  border-radius: 5px;
+  background-color: #fff;
+}
+.sidebar-inner{
+  padding:10px;
+}
+.sidebar-top{
+  padding-bottom:10px;
+  border-bottom: 1px solid #ccc;
+}
+.sidebar-min{
+  padding:10px 0;
+  line-height: 30px;
+  border-bottom: 1px solid #ccc;
+}
+.sidebar-min p{
+  text-align: left;
+  font-size:13px;
+  color:#555;
+}
+.sidebar-top p{
+  margin-top:10px;
+  text-align: center;
+  font-size: 16px;
+  color:#404a58;
+  font-weight: bold;
+}
+.avater{
+  width:120px;
+  height: 120px;
+  margin: 0 auto;
+}
+.avater img{
+  display: block;
+  max-width: 120px;
+  height:auto;
+  border-radius: 50%;
+  box-sizing: border-box;
 }
 @media screen and (max-width: 769px) {
   .index{
